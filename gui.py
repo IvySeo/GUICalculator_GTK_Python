7672e8416dbe0328f90e845dc9f619b7e803455d
Ivy Seo <ivy.rice.seo@gmail.com>
2019-08-21 13:43:47 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -10,36 +10,39 @@
 # 2. 'python3 filename.py' to start the program
 #-----------------------------------------
 
-# gui.py: for GUI using gtk
-# calcul.py: for calculation
+# main.py: for GUI using gtk
+# calcFile.py: for calculation
 
-import calcul
+#import calcFile to calculate and gtk files
+import calcul 
 import gi
-import string
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 #define
 window = Gtk.Window()
+table = Gtk.Table(4,5,True) #table size. 
 textbox = Gtk.Entry()
-table = Gtk.Table(4, 5, True)
 
-class MyWindow(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="Calculator")
+#class and def
+class MyWindow:
+	def __init__(self):
+		pass
 
-    def buttonClicked(self, widget):
-        x = textbox.get_text() #get the input from textbox
-        x = x + widget.num #helps displaying the input. w/o this, not work.
-        textbox.set_text(x) #set the text
+	def calculate(self,widget):
+		x = textbox.get_text() #get text from textbox
+		x = str(calcul.calc(x)) #send it to calcFile to calculate
+		textbox.set_text(x) #display in the textbox
 
-    def calculate(self, widget):
-        x = textbox.get_text() #to get the input from text box
-        x = str(calcul.calcu(x)) #fetch the number and send it to calculate
-        textbox.set_text(x) #display the output 
+	def buttonClicked(self,widget):
+		x = textbox.get_text()
+		x = x + widget.val #displays the input. w/o it, not work.
+		textbox.set_text(x)
 
-# creates buttons 
-btn1 = Gtk.Button(label='1') 
+w = MyWindow()
+
+# creates buttons and label them
+btn1 = Gtk.Button(label='1')
 btn2 = Gtk.Button(label='2')
 btn3 = Gtk.Button(label='3')
 btn4 = Gtk.Button(label='4')
@@ -50,45 +53,37 @@ btn8 = Gtk.Button(label='8')
 btn9 = Gtk.Button(label='9')
 btn0 = Gtk.Button(label='0')
 
-btnAdd = Gtk.Button(label='+') 
+btnAdd = Gtk.Button(label='+')
 btnSub = Gtk.Button(label='-')
-btnMul = Gtk.Button(label='x')
+btnMul = Gtk.Button(label='X')
 btnDiv = Gtk.Button(label='/')
-btnMod = Gtk.Button(label='%')
+btnExp = Gtk.Button(label='^')
 btnCal = Gtk.Button(label='=')
 
 #define value of the button
-btn1.num = '1' #value should be string, not int (bug) 
-btn2.num = '2'
-btn3.num = '3'
-btn4.num = '4'
-btn5.num = '5'
-btn6.num = '6'
-btn7.num = '7'
-btn8.num = '8'
-btn9.num = '9'
-btn0.num = '0'
+#value should be string, not int (bug) 
 
-btnAdd.num = '+'
-btnSub.num = '-'
-btnMul.num = 'x'
-btnDiv.num = '/'
-btnMod.num = '%'
-btnCal.num = '='
+#list for for-loop
+buttons = [btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9]
 
-#activate the buttons when it's clicked.
-Blist = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnAdd, btnSub, btnMul, btnDiv, btnMod] #list for for-loop
+for i in range(len(buttons)):
+	buttons[i].val=str(i)
 
-w = MyWindow() 
+btnAdd.val = '+'
+btnSub.val = '-'
+btnMul.val = 'x'
+btnDiv.val = '/'
+btnMod.val = '^'
+btnCal.val = '='
 
+#list to activate button clicks
+blists = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnAdd, btnSub, btnMul, btnDiv, btnExp, btnCal]
+for i in blists:
+	i.connect("clicked",w.buttonClicked)
 
-for i in Blist:    #used for-loop for efficiency
-    i.connect("clicked", w.buttonClicked) 
+btnCal.connect("clicked",w.calculate)
 
-btnCal.connect("clicked", w.calculate) #activates calculate def
-
-
-#locate
+#locate the button location. check the image on 'readme' on github
 table.attach(btn1, 0, 1, 1, 2)
 table.attach(btn2, 1, 2, 1, 2)
 table.attach(btn3, 2, 3, 1, 2)
@@ -103,16 +98,12 @@ table.attach(btnAdd, 0, 1, 4, 5)
 table.attach(btnSub, 2, 3, 4, 5)
 table.attach(btnMul, 3, 4, 0, 1)
 table.attach(btnDiv, 3, 4, 1, 2)
-table.attach(btnMod, 3, 4, 2, 3)
-table.attach(btnCal, 3, 4, 3, 5) #so the calculation button is bigger
+table.attach(btnExp, 3, 4, 2, 3)
+table.attach(btnCal, 3, 4, 3, 5) 
 table.attach(textbox, 0, 3, 0, 1)
 
-window.add(table) #to add the table
 
-window.show_all() #to display all the objects I created. w/o, can't see
-window.connect("delete-event", Gtk.main_quit)  #exit
-Gtk.main() #call the main()
-
-
-
-
+window.add(table)
+window.show_all() #to display all the objects I created.
+window.connect("delete-event",Gtk.main_quit) #exit
+Gtk.main()
