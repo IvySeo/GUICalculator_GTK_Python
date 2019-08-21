280de5daaa04d23580f074212b965184a334371a
Ivy Seo <ivy.rice.seo@gmail.com>
2019-08-21 13:45:15 -0500

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -1,90 +1,83 @@
-# gui.py: for GUI using gtk
-# calcul.py: for calculation
 
-class Calculate:
-    def __init__(self):
-        self.items=[]
-   
-    def push(self, value):
-        self.items.append(value)
+class calculation:
+	def __init__(self):
+		self.items=[]
 
-    def pop(self):
-        return self.items.pop()
+	def push(self,num):
+		self.items.append(num)
 
-inputValue = Calculate()
-operator = Calculate()
+	def pop(self):
+		return self.items.pop()
 
-def calcu():
-    x = inputValue.pop()
-    y = operator.pop()
-    z = inputValue.pop()
-
-    if y == '+':
-        while True:
-            try: 
-                 x = float(x)
-                 y = float(x)
-            except ValueError:
-                print("Invalid input")
-                continue
-            else:
-                break
-        inputValue.push(add(x,z))
-        
-
-    elif y == '-':
-        while True:
-            try: 
-                 x = float(x)
-                 y = float(x)
-            except ValueError:
-                print("Invalid input")
-                continue
-            else:
-                break
-        inputValue.push(subtract(x,z))
-
-    elif y == '/':
-        while True:
-            try: 
-                 x = float(x)
-                 y = float(x)
-            except ValueError:
-                print("Invalid input")
-                continue
-            else:
-                break
-        inputValue.push(divide(x,z))
-
-    elif y == 'x':
-        while True:
-            try: 
-                 x = float(x)
-                 y = float(x)
-            except ValueError:
-                print("Invalid input")
-                continue
-            else:
-                break
-        inputValue.push(multiply(x,z))
-
-    if y == '%':
-        while True:
-            try: 
-                 x = float(x)
-                 y = float(x)
-            except ValueError:
-                print("Invalid input")
-                continue
-            else:
-                break
-        inputValue.push(modular(x,z))
+inputValue = calculation()
+operator = calculation()
 
 
+def define():
+	x=inputValue.pop()
+	y=operator.pop()
+	z=inputValue.pop()
 
-    
+	if y=='*':
+            while True:
+                try:
+                except ValueError:
+                    print("Invalid input")
+                    continue
+                else:
+                    break
+		inputValue.push(add(x,z))
 
+	elif y=='/':
+            while True:
+                try:
+                except ValueError:
+                    print("Invalid input")
+                    continue
+                else:
+                    break
+		inputValue.push(z/x)
 
+	elif y=='+':
+            while True:
+                try:
+                except ValueError:
+                    print("Invalid input")
+                    continue
+                else:
+                    break
+		inputValue.push(x+z)
 
+	elif y=='-':
+            while True:
+                try:
+                except ValueError:
+                    print("Invalid input")
+                    continue
+                else:
+                    break
+		inputValue.push(z-x)
 
+	elif y=='^':
+            while True:
+                try:
+                except ValueError:
+                    print("Invalid input")
+                    continue
+                else:
+                    break
+		inputValue.push(z**x)
 
+def calc(expr):
+	num=""
+		
+	if num:
+		inputValue.push(eval(num))
+		define()
+	return inputValue.pop()
+
+				
+			
+
+		
+			
