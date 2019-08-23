b5cfd26d3c0d43c8bc4d05fefc175b909c5e9432
Ivy Seo <ivy.rice.seo@gmail.com>
2019-08-23 15:15:57 -0500

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -1,81 +1,89 @@
+"""
+----- How it works -----
+For more details, and how it's used, read the comments on each line
 
+1) def calc() is called from gui.py
+2) there is a class calculation() and 
+       inputValue = calculation() 
+       operator = calculation()
+   so each of them has their own lists
+3) inputValue.push(eval(num)) : the inputs will be appended to the list of inputValue.
+   So, the inputValue list (self.items=[]) will be added with the inputs
+   (operator input element will be saved in operator list)
+4) define() : to sort the input values and calculate
+4.1) sort and define the variables : first input number, operator, second input number
+4.2) calculate two numbers and push the value
+4.3) the calculated value will be added to the inputValue list at the last
+5) return inputValue.pop(): it will return the calculated value using pop() 
+   because the value is located at the last of the inputValue list
+   
+- pop() will delete the data/reset after use (the list will be empty)
+"""
+
+# class and def
 class calculation:
 	def __init__(self):
-		self.items=[]
+		self.items=[] # list
+		
+	def errorTest(self): # value error test
+	    while True:
+                try:
+                except ValueError:
+                    print("Invalid input")
+                    continue
+                else:
+                    break
 
-	def push(self,num):
-		self.items.append(num)
+	def push(self, num):
+		self.items.append(num)  # appends a num to the end of the self.items list
 
 	def pop(self):
-		return self.items.pop()
+		return self.items.pop() 
 
-inputValue = calculation()
+inputValue = calculation() 
 operator = calculation()
-
+# These will have their own list
 
 def define():
-	x=inputValue.pop()
-	y=operator.pop()
-	z=inputValue.pop()
+     # save the last input elememt of the list into a variable (x, y, z)
+     # and pop that element out (remove) from the list 
+	x = inputValue.pop()  # the second input number (= the last from the list of inputValue list)
+	y = operator.pop()    # operator input (the last/only element of the list of operator list)
+	z = inputValue.pop()  # the first input number
 
-	if y=='*':
-            while True:
-                try:
-                except ValueError:
-                    print("Invalid input")
-                    continue
-                else:
-                    break
-		inputValue.push(add(x,z))
+	if y=='x':            # if the operator element is equal to 'x',
+                errorTest()   # run the value error test
+		inputValue.push(add(x,z)) 
+		# x,z will be calculated and pushed to push()
+		# then the calculated value will be added to the inputValue list at the last of the list
+		# so when'return inputValue.pop()' is called, the calculated value will be returned
 
 	elif y=='/':
-            while True:
-                try:
-                except ValueError:
-                    print("Invalid input")
-                    continue
-                else:
-                    break
-		inputValue.push(z/x)
+                errorTest()
+		inputValue.push(z/x) #since z is the first input number, z comes first
 
 	elif y=='+':
-            while True:
-                try:
-                except ValueError:
-                    print("Invalid input")
-                    continue
-                else:
-                    break
+                errorTest()
 		inputValue.push(x+z)
 
 	elif y=='-':
-            while True:
-                try:
-                except ValueError:
-                    print("Invalid input")
-                    continue
-                else:
-                    break
+                errorTest()
 		inputValue.push(z-x)
 
 	elif y=='^':
-            while True:
-                try:
-                except ValueError:
-                    print("Invalid input")
-                    continue
-                else:
-                    break
+                errorTest()
 		inputValue.push(z**x)
 
+# gui.py calls this one 
 def calc(expr):
-	num=""
+	num="" 
 		
-	if num:
-		inputValue.push(eval(num))
-		define()
-	return inputValue.pop()
-
+        inputValue.push(eval(num))   #eval() method returns the result evaluated from the expression
+        define()                     #call the define() to calculate
+	
+	return inputValue.pop() 
+# returns the value to gui.py 
+# value = the last element from the list, which is the calculated value
 				
 			
 
